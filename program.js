bf42ea45480b68adf99c76b978909d9c5a1a0c8a
Paulo César Coronado <paulocoronado@gmail.com>
2025-09-05 15:19:12 -0500

    v 0.1 Mostrar map

---
+++ program.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.629148723033761, -74.06547217080218], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
